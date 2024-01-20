2e1a3746a2195d395cf58251aafab0d163981d2e
newtoallofthis123 <noobscience123@gmail.com>
2024-01-20 18:20:06 +0530

    Fix some styles

--- components/tabs/survey.php
+++ components/tabs/survey.php
@@ -22,19 +22,38 @@
                 Note the obstacles, check if the obstacles disqualify the goal, and check if the goal is still worth pursuing.
             </p>
         </div>
-        <div class="input-group mb-2" id="input-group-1">
-            <div class="input-group-text">
-                <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                <span class="ps-3 pb-1">
-                    Disqualify
-                </span>
+        <p>
+            Points of Interest for new Destinations
+        </p>
+        <div id="Interests">
+            <div id="input-interest-1" class="input-group flex-nowrap">
+                <span class="input-group-text" id="addon-wrapping-1">1</span>
+                <input type="text" class="form-control" placeholder="Add a Point of Interest" aria-label="interest" aria-describedby="addon-wrapping-1">
             </div>
-            <input type="text" class="form-control" aria-label="Text input with checkbox" id="obstacle-input-1" placeholder="Add an Obstacle">
+        </div>
+        <div class="pt-3">
+            <button type="button" class="btn btn-primary ms-3" id="add-interest-group">Add New Interest</button>
+            <button type="button" class="btn btn-secondary ms-3" id="remove-interest-group">Remove Last Point</button>
+        </div>
+        <p class="pt-4">
+            Points of Concern and Hazards
+        </p>
+        <div id="input-group-1" class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping-1">1</span>
+            <input type="text" class="form-control" placeholder="Add an obstacle" aria-label="requirement" aria-describedby="addon-wrapping-1">
         </div>
     </form>
     <div>
-        <button type="button" class="btn btn-info ms-3" id="add-obstacle">Add New Point</button>
-        <button type="button" class="btn btn-secondary ms-3" id="remove-obstacle">Remove Last Point</button>
+        <button type="button" class="btn btn-info ms-3" id="add-obstacle">Add New Obstacle</button>
+        <button type="button" class="btn btn-secondary ms-3" id="remove-obstacle">Remove Last Obstacle</button>
+    </div>
+    <div class="p-3">
+        <p class="fs-5">
+            Found a problem?
+        </p>
+        <button class="btn btn-danger" type="button">
+            💀 Give Up
+        </button>
     </div>
 </div>
 <script>
@@ -43,14 +62,9 @@
         $("#add-obstacle").click(function() {
             group_no++;
             $("#Survey form").append(`
-            <div class="input-group mb-2" id="input-group-${group_no}">
-                <div class="input-group-text">
-                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                    <span class="ps-3 pb-1">
-                        Disqualify
-                    </span>
-                </div>
-                <input type="text" class="form-control" aria-label="Text input with checkbox" id="obstacle-input-${group_no}">
+            <div id="input-group-${group_no}" class="input-group flex-nowrap">
+                <span class="input-group-text" id="addon-wrapping-${group_no}">${group_no}</span>
+                <input type="text" class="form-control" placeholder="Add a Requirement" aria-label="requirement" aria-describedby="addon-wrapping-${group_no}">
             </div>
             `);
         });
